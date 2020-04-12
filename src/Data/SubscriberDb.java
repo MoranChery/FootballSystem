b6d47ed20b80c205a01cbe36ead3f47fd43d9020
noyharar <noyharar@post.bgu.ac.il>
2020-04-12 21:06:39 +0300

    Merge with Hila

--- src/Data/SubscriberDb.java
+++ src/Data/SubscriberDb.java
@@ -3,4 +3,6 @@ package Data;
 public interface SubscriberDb {
         void createSubscriber(Model.UsersTypes.Subscriber subscriber) throws Exception;
         Model.UsersTypes.Subscriber getSubscriber(String username) throws Exception;
+
+        void deleteAll();
 }
--- src/Data/SubscriberDbInMemory.java
+++ src/Data/SubscriberDbInMemory.java
@@ -1,20 +1,14 @@
 package Data;
-<<<<<<< HEAD
-
-import Model.UsersTypes.Subscriber;
-import Model.UsersTypes.TeamOwner;
-=======
-import Model.UsersTypes.Subscriber;
->>>>>>> 5c8d1e9a788270f33fd370de7e0a433d3d2e0c1d
 
 import java.util.HashMap;
 import java.util.Map;
+import Model.UsersTypes.Subscriber;
 
 public class SubscriberDbInMemory implements SubscriberDb {
     /*structure like the DB of subscriber*/
     //key: username
     //value: subscriber class
-    private Map<String, Model.UsersTypes.Subscriber> subscriberMap;
+    private Map<String, Model.UsersTypes.Subscriber> subscribers;
 
     public SubscriberDbInMemory() {
         this.subscribers = new HashMap<>();
@@ -26,9 +20,6 @@ public class SubscriberDbInMemory implements SubscriberDb {
         return ourInstance;
     }
 
-    public SubscriberDbInMemory() {
-        subscriberMap = new HashMap<>();
-    }
 
     /**
      * for the tests - create player in DB
@@ -37,22 +28,24 @@ public class SubscriberDbInMemory implements SubscriberDb {
      */
     @Override
     public void createSubscriber(Subscriber subscriber) throws Exception {
-        if(subscriberMap.containsKey(subscriber.getUsername())) {
+        if(subscribers.containsKey(subscriber.getUsername())) {
             throw new Exception("subscriber already exists");
         }
-        subscriberMap.put(subscriber.getUsername(), subscriber);
+        subscribers.put(subscriber.getUsername(), subscriber);
     }
 
 
     @Override
     public Subscriber getSubscriber(String username) throws Exception {
-        if (!subscriberMap.containsKey(username)) {
+        if (!subscribers.containsKey(username)) {
             throw new Exception("Subscriber not found");
         }
-        return subscriberMap.get(username);
+        return subscribers.get(username);
     }
+
     @Override
     public void deleteAll() {
+
         subscribers.clear();
     }
 }
