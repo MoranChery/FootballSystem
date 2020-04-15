d180901415c0f4a70abd7c95e7a78f3f04bbee18
gorenNoa <noagoren@post.bgu.ac.il>
2020-04-15 17:24:10 +0300

    game id

--- src/Model/UsersTypes/Judge.java
+++ src/Model/UsersTypes/Judge.java
@@ -11,6 +11,7 @@ public class Judge extends Subscriber
     private QualificationJudge qualificationJudge;
     private List<Integer> inlaySeasonLeagueIdList;
 
+
     public Judge(String username, String password,Integer id, String firstName, String lastName, QualificationJudge qualificationJudge) {
         setRegisteringDetails(username,password,id,firstName,lastName);
         this.qualificationJudge = qualificationJudge;
