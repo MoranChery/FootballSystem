d3d7d7c29e925f200163080337575eaab6507d7b
gorenNoa <noagoren@post.bgu.ac.il>
2020-04-24 03:39:13 +0300

    SubscriberServiceTest + JudgeServiceTest + FanService tests - 100%

--- src/Service/FanService.java
+++ src/Service/FanService.java
@@ -14,23 +14,17 @@ public class FanService {
         this.fanController = new FanController();
     }
 
+    public Fan getFan(String fanMail) throws Exception{
+        return fanController.getFan(fanMail);
+    }
 
+    public void createFan(Fan fan) throws Exception {
+        fanController.createFan(fan);
+    }
     public void askToGetAlerts(String fanMail, AlertWay alertWay) throws Exception {
         fanController.askToGetAlerts(fanMail,alertWay);
     }
     public void addPageToFanListOfPages(String fanMail, String pageID) throws Exception{
         fanController.addPageToFanListOfPages(fanMail, pageID);
     }
-
-//    public void logOut(String fanMail, Status status) throws Exception{
-//        fanController.logOut(fanMail, status);
-//    }
-//    public void askToGetAlerts(String fanMail, GamesAlert alert, AlertWay alertWay) throws Exception {
-//        fanController.askToGetAlerts(fanMail,alert,alertWay);
-//    }
-
-
-//    public void editPersonalDetails(String fanMail,String password, Integer id, String firstName, String lastName) throws Exception {
-//        fanController.editPersonalDetails(fanMail, password, id, firstName, lastName);
-//    }
 }
