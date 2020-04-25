c276a9662ffb38090bc4c43c4e2eecb4cb70b9af
gorenNoa <noagoren@post.bgu.ac.il>
2020-04-25 19:56:35 +0300

    User tests - 100%

---
+++ test/Service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package Service;
+
+import Data.Db;
+import Data.GameDbInMemory;
+import Data.JudgeDb;
+import Data.JudgeDbInMemory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceTest {
+
+    private UserService userService = new UserService();
+
+    @Test
+    public void showInfoTest(){
+        //TODO
+        userService.showInfo();
+    }
+    @Test
+    public void searchInfoTest(){
+        //TODO
+        userService.searchInfo();
+    }
+
+}
